9851c6b27956a0b82378cc43fe39f879f187c629
Alexey Lagun <alexey.lagun@gmail.com>
2024-01-04 03:09:15 +0300

    feat: add solution depth

--- app/src/tasks/Task12/depth.ts
+++ app/src/tasks/Task12/depth.ts
@@ -1,6 +1,13 @@
 type NestedArray<T> = Array<T | NestedArray<T>>;
+
 const depth = (array: NestedArray<number>) => {
   try {
+    for (let index = 0; index < array.length; index++) {
+      if (Array.isArray(array[index])) {
+        index = 0;
+        array = array.flat();
+      }
+    }
     return array;
   } catch (error) {
     console.log("Error:", error);
